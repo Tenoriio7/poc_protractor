a73644739941d2213f9469ae221df7c61b754c5b
Vinicius Tenorio <viniciustenorio@viniciustenorio.local>
2017-06-26 15:55:40 -0300

    second commit

---
+++ specs.js
@@ -0,0 +1,25 @@
+browser.ignoreSynchronization = true;
+var helper = require('./helper')
+describe ('Logos', function() {
+  xit('should have the blog logo', function() { //xit faz com que ignore o teste
+   browser.get('https://stackoverflow.com');
+   helper.verifyElementPresenceById('h_search-input')
+  });
+  it ('should navigate to the first blog post',function (){
+    var ask = $('.btn-outlined');
+    var signup = $('.val-textemphasis');
+    browser.get('https://stackoverflow.com/').then(function(){
+      expect(ask.isDisplayed()).toBeTruthy();
+      ask.click();
+      browser.manage().timeouts().pageLoadTimeout(10000)
+      expect(signup.isDisplayed()).toBeTruthy();
+      signup.click();
+      expect(signup.getText()).toContain
+      ('You must be logged in to ask a question on Stack Overflow')
+    });
+  });
+  xit ('should have the website logo' , function(){
+     browser.get('https://www.submarino.com.br/')
+     helper.verifyElementPresenceById('h_search-input')
+  });
+});
